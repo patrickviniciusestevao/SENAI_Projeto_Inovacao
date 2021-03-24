4a17e4e2cbd130837306a7a6f53944ca8014e309
n47iv0 <lucasmatos.bis.lm@gmail.com>
2021-03-24 14:14:03 -0300

    Teste Lucas

--- SENAI Requerimento Padrao/CODE/MODULOS/USUARIOS/BLL/UsuarioBLL.cs
+++ SENAI Requerimento Padrao/CODE/MODULOS/USUARIOS/BLL/UsuarioBLL.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 using SENAI_Requerimento_Padrao.CODE.FUNCTIONS;
 
 namespace SENAI_Requerimento_Padrao.CODE.BLL
-{
+{//teste lucas
 	class UsuarioBLL
 	{
 		AcessoBancoDados bd;
